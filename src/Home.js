08fa6b8f912a4622ecc7d2e9ce652b182ff20fa1
Imran Khan <imran.khan@kpn.com>
2022-01-30 16:12:47 +0100

    #18 conditional loading message

--- src/Home.js
+++ src/Home.js
@@ -5,48 +5,29 @@ import BlogList from "./BlogList";
 const Home = () => {
   // Outputting lists
   const [blogs, setBlogs] = useState(null)
-
-  const [name, setName] = useState('Imran');
+  const [isPending, setIsPending] = useState(true);
 
   // Fires on every render
   useEffect(() => {
-    fetch('http://localhost:8000/blogs')
-        .then(res => {
-          return res.json();
-        })
-        .then(data => {
-          console.log('data', data);
-          setBlogs(data);
-        });
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
+          .then(res => {
+            return res.json();
+          })
+          .then(data => {
+            console.log('data', data);
+            setBlogs(data);
+            setIsPending(false);
+          });
+      }, 3000);
   }, []);
-
-  // const handleDelete = (id) => {
-  //   const newBlogs = blogs.filter((blog) => blog.id !== id)
-  //   setBlogs(newBlogs)
-  // };
-
-  // useState: Reactive state
-  // const [name, setName] = useState('Mahreen');
-  // const [age, setAge] = useState(36);
-
-  // const handleClick = () => {
-  //   setName('Musa');
-  //   setAge(1)
-  // }
-
-  // const handleClickAgain = (name, e) => {
-  //   console.log('Hi', name, e.target);
-  // }
 
   return (
       <div className="home">
+        {/* Pending state */}
+        {isPending && <div>Loading</div>}
+        {/* Blog data */}
         {blogs &&  <BlogList hasBlogs={blogs} title="All blogs"/>}
-        <button onClick={() => setName('Musa')}>Change name</button>
-        <p>{ name }</p>
-        {/*<p>{name} is {age} year(s) of age</p>*/}
-        {/*<button onClick={handleClick}>Click</button>*/}
-        {/* Event objects */}
-        {/*<button onClick={(e) => handleClickAgain('Imran', e)}>Click again</button>*/}
       </div>
   );
 };
