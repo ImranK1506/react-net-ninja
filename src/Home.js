1b1d4269b72ea19ceb547e0f7cc9d5acc05b42fd
Imran Khan <imran.khan@kpn.com>
2022-01-30 16:30:37 +0100

    #19 handle errors

--- src/Home.js
+++ src/Home.js
@@ -4,28 +4,37 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   // Outputting lists
-  const [blogs, setBlogs] = useState(null)
+  const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   // Fires on every render
   useEffect(() => {
     setTimeout(() => {
       fetch('http://localhost:8000/blogs')
           .then(res => {
-            return res.json();
+            console.log(res);
+            if (!res.ok) throw Error('Could not fetch data')
+            return res.json()
           })
           .then(data => {
-            console.log('data', data);
             setBlogs(data);
             setIsPending(false);
-          });
+            setError(null);
+          })
+          .catch((err) => {
+            setIsPending(false);
+            setError(err.message);
+          })
       }, 3000);
   }, []);
 
   return (
       <div className="home">
+        {/* Handle error */}
+        {error && <div>{ error }</div>}
         {/* Pending state */}
-        {isPending && <div>Loading</div>}
+        {isPending && <div>Loading...</div>}
         {/* Blog data */}
         {blogs &&  <BlogList hasBlogs={blogs} title="All blogs"/>}
       </div>
