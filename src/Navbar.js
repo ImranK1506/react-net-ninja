e0845acc5f21e8f572de0057309d652250084b58
Imran Khan <imran.khan@kpn.com>
2022-01-30 19:52:54 +0100

    #23 router links

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
       <div>
         <nav className="navbar">
           <h1>Ninja blog</h1>
           <div className="links">
-            <a href="/">Home</a>
-            <a href="/create">New blog</a>
+            <Link to="/">Home</Link>
+            <Link to="/create">New blog</Link>
           </div>
         </nav>
       </div>
